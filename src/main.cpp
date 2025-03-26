feb5788a092b2febb31fe0aae47d9069d62025d1
ruze42 <malledeferlive@gmail.com>
2025-03-26 22:20:50 +0500

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,19 @@
 
 using namespace std;
 
-#define HIGH 1
-#define DIGITS 1
-#define SIGNS 1
-
-string GenPass(long long length,bool h,bool d,bool s)
+string
+gen_pass(long long length, bool h,bool d,bool s)
 {
     srand (time(NULL));
 	string chars, pass;
 
-	if(h)
-		chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	if(d)
-		chars += "0123456789";	
+	if(h) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	if(s)
-		chars += "()/*-+?!@#$^&*_=<>[]{}:;,.";
+	if(d) chars += "0123456789";	
 
-	for (short i = 0; i < length; i++)
+	if(s) chars += "()/*-+?!@#$^&*_=<>[]{}:;,.";
+
+	for (long long i = 0; i < length; i++)
     {
         char ch = chars[rand() % chars.length()];
 		pass.push_back(ch);
@@ -33,24 +28,20 @@ string GenPass(long long length,bool h,bool d,bool s)
     return pass;
 }
 
-int main(int argc, char* argv[])
+int 
+main(int argc, char* argv[])
 {
-        long long length = 8;
+    long long length = 8;
+    bool digits = true;
+    bool hight = true;
+    bool signs = true;
 
-        bool digits = true;
-        bool hight = true;
-        bool signs = true;
+    if (argc > 1)
+    {
+        length = (long long)argv[1];
+        cout << length;
+    }
 
-        /* settings[0] is high register characters
-         * settings[1] is digits
-         * settings[2] is signs
-         */ 
-
-        cout << GenPass(length,hight,digits,signs) << "\n";
-
-		#ifdef _WIN32
-			system("pause");
-		#endif
-
-        return 0;
+    cout << gen_pass(length, hight, digits, signs) << "\n";
+    return 0;
 }
