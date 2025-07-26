3e0185aeebeab9bef55a9136915733ed556f8454
ruzen42 <malledeferlive@gmail.com>
2025-07-26 11:23:40 +0500

    fix all

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,34 @@ module Main where
 
 import RandomGenerator
 import Control.Concurrent.Async (wait)
+import Options.Applicative
+
+data Options = Options
+  { passwordSize :: Int 
+  }
 
 main :: IO ()
-main = do 
-  task <- RandomGenerator.generatePasswordAsync 8
-  password <- wait task 
+main = do
+  options <- execParser parserInfo
+  let size = passwordSize options
+  task <- RandomGenerator.generatePasswordAsync size
+  password <- wait task
   putStrLn password
+
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> option auto 
+    ( long "lenght"
+   <> short 'l'
+   <> metavar "LENGHT" 
+   <> help "Length for your password"
+   <> showDefault
+   <> value 8 
+    )
+
+parserInfo :: ParserInfo Options
+parserInfo = info (optionsParser <**> helper)
+  ( fullDesc
+ <> progDesc "simple program to generate password"
+ <> header "fastpass - password generator"
+  )
